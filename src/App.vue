a231279a85b99e39ace22e5bf0300409229b0304
Papper <oluwabukunmi62@gmail.com>
2020-06-22 14:42:46 +0100

    initial

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h1>filters and mixins</h1>
+    <p>{{ text | toUppercase }}</p>
+    <input v-model="filterText" />
+    <ul>
+      <li v-for="(fruit, index) in filteredFruits" :key="index">{{ fruit }}</li>
+    </ul>
+    <hr />
+    <app-list></app-list>
+  </div>
+</template>
+
+<script>
+import List from "./List.vue";
+import { fruitMixin } from "./fruitMixin.js";
+export default {
+  mixins: [fruitMixin],
+  filters: {
+    toUppercase(value) {
+      return value.toUpperCase();
+    }
+  },
+  components: {
+    appList: List
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/List.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>filters and mixins</h1>
+    <input v-model="filterText">
+    <ul>
+      <li v-for="(fruit, index) in filteredFruits" :key="index">{{fruit}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import {fruitMixin} from './fruitMixin.js'
+export default{
+  mixins: [fruitMixin]
+};
+</script>
